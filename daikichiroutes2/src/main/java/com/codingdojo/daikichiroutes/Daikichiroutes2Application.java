591ac51fd27455a9801353762538d5142ddd9318
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-10-30 14:34:49 +0200

    add

---
+++ daikichiroutes2/src/main/java/com/codingdojo/daikichiroutes/Daikichiroutes2Application.java
@@ -0,0 +1,30 @@
+package com.codingdojo.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class Daikichiroutes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daikichiroutes2Application.class, args);
+	}
+	@RequestMapping("/daikichi")
+	public String welcome() {
+		return "welcome";
+	}
+	@RequestMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck in all your endovers";
+	}
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow,an opportunity will a rise,so be open to new ideas!";
+	}
+	
+
+}
---
+++ daikichiroutes2/src/test/java/com/codingdojo/daikichiroutes/Daikichiroutes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daikichiroutes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
